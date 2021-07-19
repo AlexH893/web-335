9d81a715c4be3d225ac03895b481d5a5572a6f2c
Alex <31377763+AlexH893@users.noreply.github.com>
2021-07-19 18:17:27 -0500

    Create haefner_calculator.py

---
+++ week-8/haefner_calculator.py
@@ -0,0 +1,13 @@
+def add_function(p1, p2):
+    return p1 + p2;
+
+def sub_function(p1, p2):
+    return p1 - p2;
+
+def div_function(p1, p2):
+    return p1 / p2;
+
+print(add_function(1, 2))
+print(sub_function(1, 2))
+print(div_function(1, 2))
+
